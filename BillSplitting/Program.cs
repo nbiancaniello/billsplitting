e7344283c9789b989802e227a50c24f122512948
Nicolas Biancaniello <nicolasbiancaniello@gmail.com>
2018-08-06 15:03:39 -0400

    Add project files.

---
+++ BillSplitting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillSplitting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
